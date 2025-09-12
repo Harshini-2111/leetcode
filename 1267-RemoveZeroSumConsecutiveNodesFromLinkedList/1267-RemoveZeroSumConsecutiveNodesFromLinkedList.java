eadd95efa352433fac6bff00cbe3dfcbc6aaf189
Harshini Pechetti <23mh1a05o0@acoe.edu.in>
2025-09-12 19:22:37 +0530

    Create 1267-RemoveZeroSumConsecutiveNodesFromLinkedList

---
+++ 1267-RemoveZeroSumConsecutiveNodesFromLinkedList/1267-RemoveZeroSumConsecutiveNodesFromLinkedList.java
@@ -0,0 +1,41 @@
+// Last updated: 12/09/2025, 19:22:37
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeZeroSumSublists(ListNode head) {
+        Map<Integer, ListNode> map = new HashMap<>();
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        map.put(0, dummy);
+        int prefixSum = 0;
+        while (head != null) {
+            prefixSum += head.val;
+            if (!map.containsKey(prefixSum)) {
+                map.put(prefixSum, head);
+            }
+            else {
+                ListNode start = map.get(prefixSum);
+                ListNode curr = start;
+                int sum = prefixSum;
+                while (curr != head) {
+                    curr = curr.next;
+                    sum += curr.val;
+                    if (curr != head) {
+                        map.remove(sum);
+                    }
+                }
+                start.next = head.next;
+            }
+            head = head.next;
+        }   
+        return dummy.next;
+    }
+}
