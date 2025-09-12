c1293abd5fed53874417d1f279b9867520fef25f
Harshini Pechetti <23mh1a05o0@acoe.edu.in>
2025-09-12 19:18:24 +0530

    Create 3194-FindWordsContainingCharacter

---
+++ 3194-FindWordsContainingCharacter/3194-FindWordsContainingCharacter.java
@@ -0,0 +1,13 @@
+// Last updated: 12/09/2025, 19:18:24
+class Solution {
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].contains(String.valueOf(x))) {
+                result.add(i);
+            }
+        }
+        
+        return result;
+    }
+}
