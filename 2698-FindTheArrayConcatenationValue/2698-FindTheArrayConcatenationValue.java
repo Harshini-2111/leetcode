395e9efcbe34172aa5ee66b18bc197b756db9dab
Harshini Pechetti <23mh1a05o0@acoe.edu.in>
2025-09-12 19:18:27 +0530

    Create 2698-FindTheArrayConcatenationValue

---
+++ 2698-FindTheArrayConcatenationValue/2698-FindTheArrayConcatenationValue.java
@@ -0,0 +1,15 @@
+// Last updated: 12/09/2025, 19:18:27
+class Solution {
+    public long findTheArrayConcVal(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        long total = 0;
+        while (left < right) {
+            String conVal = String.valueOf(nums[left]) + String.valueOf(nums[right]);
+            total += Long.parseLong(conVal);
+            left++;
+            right--;
+        }
+        return (nums.length % 2 == 0) ? total : total + Long.valueOf(nums[left]);
+    }
+}
