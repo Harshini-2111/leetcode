b6057873348c45ed86432986a1d83d73381d12b7
Harshini Pechetti <23mh1a05o0@acoe.edu.in>
2025-09-12 19:18:44 +0530

    Create 1993-SumOfAllSubsetXorTotals

---
+++ 1993-SumOfAllSubsetXorTotals/1993-SumOfAllSubsetXorTotals.java
@@ -0,0 +1,19 @@
+// Last updated: 12/09/2025, 19:18:44
+class Solution {
+    public int subsetXORSum(int[] nums) {
+        int n = nums.length;
+        int total = 0;
+        for (int mask = 0; mask < (1 << n); mask++) {
+            int xor = 0;
+            for (int i = 0; i < n; i++) {
+                if ((mask & (1 << i)) != 0) {
+                    xor ^= nums[i];
+                }
+            }
+            
+            total += xor;
+        }
+        
+        return total;
+    }
+}
