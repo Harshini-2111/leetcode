edfd7d11b4f296e72add81ef9b678ba309480d8a
Harshini Pechetti <23mh1a05o0@acoe.edu.in>
2025-09-12 19:18:22 +0530

    Create 3241-DivideArrayIntoArraysWithMaxDifference

---
+++ 3241-DivideArrayIntoArraysWithMaxDifference/3241-DivideArrayIntoArraysWithMaxDifference.cpp
@@ -0,0 +1,16 @@
+// Last updated: 12/09/2025, 19:18:23
+class Solution {
+public:
+    vector<vector<int>> divideArray(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+
+        vector<vector<int>> res;
+        for(int i=2;i<n;i+=3){
+            if(nums[i]-nums[i-2]>k)
+                return {};
+            res.push_back({nums[i-2],nums[i-1],nums[i]});
+        }
+        return res;
+    }
+};
