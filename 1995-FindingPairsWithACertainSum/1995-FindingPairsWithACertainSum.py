d8502d0d5511ca9723f9f9c21443d69d06d1b397
Harshini Pechetti <23mh1a05o0@acoe.edu.in>
2025-09-12 19:18:43 +0530

    Create 1995-FindingPairsWithACertainSum

---
+++ 1995-FindingPairsWithACertainSum/1995-FindingPairsWithACertainSum.py
@@ -0,0 +1,22 @@
+# Last updated: 12/09/2025, 19:18:43
+class FindSumPairs:
+    def __init__(self, nums1: List[int], nums2: List[int]):
+        self.arr1 = nums1
+        self.arr2 = nums2.copy()
+        self.ele_freq = defaultdict(int)
+        for ele in nums2:
+            self.ele_freq[ele] += 1
+    
+    def add(self, index: int, val: int) -> None:
+        old_val = self.arr2[index]
+        self.ele_freq[old_val] -= 1
+        self.arr2[index] += val
+        new_val = self.arr2[index]
+        self.ele_freq[new_val] += 1
+    
+    def count(self, tot: int) -> int:
+        res = 0
+        for ele in self.arr1:
+            target = tot - ele
+            res += self.ele_freq.get(target, 0)
+        return res
