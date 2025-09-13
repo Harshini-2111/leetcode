d635de9ec2a83b25dbc43f5163d7f26919878895
Harshini Pechetti <23mh1a05o0@acoe.edu.in>
2025-09-13 11:54:01 +0530

    Create 1469-MinimumNumberOfStepsToMakeTwoStringsAnagram

---
+++ 1469-MinimumNumberOfStepsToMakeTwoStringsAnagram/1469-MinimumNumberOfStepsToMakeTwoStringsAnagram.java
@@ -0,0 +1,26 @@
+// Last updated: 13/09/2025, 11:54:01
+class Solution {
+    public int minSteps(String s, String t) {
+        Map<Character,Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (map.containsKey(ch)) {
+                map.put(ch, map.get(ch) + 1);
+            }
+            else {
+                map.put(ch, 1);
+            }
+        }
+        int count = 0;
+        for (int i = 0; i < t.length(); i++) {
+            char ch = t.charAt(i);
+            if (map.containsKey(ch) && map.get(ch) > 0) {
+                map.put(ch, map.get(ch) - 1);
+            }
+            else {
+                count++;
+            }
+        }
+        return count;
+    }
+}
