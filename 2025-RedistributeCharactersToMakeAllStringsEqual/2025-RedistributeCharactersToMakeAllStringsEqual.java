323c9db125ef43bf1083bb77044941476948220d
Harshini Pechetti <23mh1a05o0@acoe.edu.in>
2025-09-24 17:28:58 +0530

    Create 2025-RedistributeCharactersToMakeAllStringsEqual

---
+++ 2025-RedistributeCharactersToMakeAllStringsEqual/2025-RedistributeCharactersToMakeAllStringsEqual.java
@@ -0,0 +1,20 @@
+// Last updated: 24/09/2025, 17:28:58
+class Solution {
+    public boolean makeEqual(String[] words) {
+        if (words.length == 1) {
+            return true;
+        }
+        int[] freq = new int[26];
+        for (String word : words) {
+            for (int i = 0; i < word.length(); i++) {
+                freq[word.charAt(i) - 'a']++;
+            }
+        }
+        for (int i : freq) {
+            if (i % (words.length) != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
