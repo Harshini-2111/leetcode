5acd5bd80d5fcea317022667830ff12d7ed79d1a
Harshini Pechetti <23mh1a05o0@acoe.edu.in>
2025-09-24 11:03:02 +0530

    Create 1765-MergeInBetweenLinkedLists

---
+++ 1765-MergeInBetweenLinkedLists/1765-MergeInBetweenLinkedLists.java
@@ -0,0 +1,20 @@
+// Last updated: 24/09/2025, 11:03:02
+class Solution {
+    public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
+        ListNode start = null;
+        ListNode end = list1;
+        for (int index = 0; index < b; index++) {
+            if (index == a - 1) {
+                start = end;
+            }
+            end = end.next;
+        }
+        start.next = list2;
+        while (list2.next != null) {
+            list2 = list2.next;
+        }
+        list2.next = end.next;
+        end.next = null;
+        return list1;
+    }
+}
